8efdb455db9d7e2af5f79d34e5e6d7cd1bea1bc8
Alexander Stasyuk <predator12007@mail.ru>
2024-03-05 01:26:12 +0300

    add polymorphic typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
 
 export function App() {
   return (
     <div>
       <Button>Hello</Button>
       <Button as={'a'}>Hello</Button>
+      <Typography as={'a'} colorBalance={500} colorTheme={'danger'} variant={'link2'}>
+        Hi
+      </Typography>
     </div>
   )
 }
