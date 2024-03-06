eb42916ee0763ad59a1de51b749345c51ed57a35
Alexander Stasyuk <predator12007@mail.ru>
2024-03-06 23:47:22 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { CustomTabs, TabsContent } from '@/components/ui/tabs'
 import { Typography } from '@/components/ui/typography'
 
 export function App() {
@@ -9,6 +10,18 @@ export function App() {
       <Typography as={'a'} colorBalance={500} colorTheme={'danger'} variant={'link2'}>
         Hi
       </Typography>
+      <CustomTabs
+        defaultValue={'tab1'}
+        tabs={[
+          { title: 'Switcher', value: 'tab1' },
+          { title: 'Switcher', value: 'tab2' },
+        ]}
+      >
+        <TabsContent value={'tab1'}>
+          Onecwcwcdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+        </TabsContent>
+        <TabsContent value={'tab2'}>Two</TabsContent>
+      </CustomTabs>
     </div>
   )
 }
